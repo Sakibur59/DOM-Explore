2d65b51c872d554efd4cbaa481c2f9be57aa0744
Sakibur59 <sakiburrahman5978@gmail.com>
2026-02-14 13:00:32 +0600

    create h3 and ul

--- script.js
+++ script.js
@@ -17,4 +17,13 @@ let create = document.createElement("section");
 main.appendChild(create);
 
 create.innerHTML= `<h1>Create element via JS</h1> 
-<p>Explore HTML DOM manipulation</p> <button>Read More</button>`
+<p>Explore HTML DOM manipulation</p> <button>Read More</button>
+
+<h3>I want to eat </h3>
+<ul>
+    <li>Mango</li>
+    <li>Orange</li>
+    <li>Jackfruit</li>
+    <li>Apple</li>
+    <li>Guava</li>
+</ul>`
