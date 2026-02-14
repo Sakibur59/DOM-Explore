fd1b0cd24ca0189a38bb272842e20aa659fc15ef
Sakibur59 <sakiburrahman5978@gmail.com>
2026-02-14 12:57:33 +0600

    easy way to create html element via JS

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ li.innerText="Mango";
 ul.appendChild(li);
 
 section.appendChild(ul)
+
+// 2nd way to create HTML element 
+
+let create = document.createElement("section");
+main.appendChild(create);
+
+create.innerHTML= `<h1>Create element via JS</h1> 
+<p>Explore HTML DOM manipulation</p> <button>Read More</button>`
