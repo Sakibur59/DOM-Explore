db167f4c7840aa4530116769c702b68f502f5ea6
Sakibur59 <sakiburrahman5978@gmail.com>
2026-02-15 19:58:01 +0600

    creating comment box and display

--- Event Handler/event.js
+++ Event Handler/event.js
@@ -2,3 +2,15 @@ document.getElementById("btn").addEventListener('click',function (){
     let btn = document.getElementById("heading");
     btn.innerText="Change the heading title";
 })
+
+document.getElementById("btn-2").addEventListener('click',function (){
+    let comment = document.getElementById("comment-box")
+    let displayValue= comment.value;
+
+    let displayOutput = document.getElementById("comment-container")
+    let commentElement = document.createElement('p');
+    commentElement.classList.add('comment');
+    commentElement.innerText = displayValue;
+    displayOutput.appendChild(commentElement);
+    
+})
